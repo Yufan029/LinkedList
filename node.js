201ccb5502a149a8f0b97459c4966d7d981d8d63
Yufan Zhang <YZhang@rpmglobal.com>
2024-11-13 20:52:24 +1000

    Implementation insertAt, removeAt, find, contain, etc.

--- node.js
+++ node.js
@@ -1,10 +1,12 @@
 export default class Node {
-    constructor (value = null, nextNode = null) {
-        this.value = value;
-        this.nextNode = nextNode;
-    }
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
 
-    toString() {
-        return `{ value: '${this.value}', nextNode: '${this.nextNode}')`;
-    }
+  toString() {
+    return `{ value: '${this.value}', nextNode: '${
+      this.nextNode === null ? "null" : this.nextNode.value
+    }')`;
+  }
 }
